4dd5e41ddf1b3cd8dbf41913e31df281c3a31643
Pilchh <alex@thepilchers.com>
2026-01-29 22:27:32 +0000

    Initial commit

---
+++ src/client/java/pilchh/evensplit/EvensplitClient.java
@@ -0,0 +1,10 @@
+package pilchh.evensplit;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EvensplitClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
